bc2c8d9eb0a313ee82afd1a518d20ad582c80739
Kapil Chaudhary <153476703+kapilchaudharygithub@users.noreply.github.com>
2023-12-31 15:01:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import { Clock } from "./JS/clock.js";
 import { GenerateOptions } from "./JS/generateOptions.js";
 import { Alarm } from "./JS/alarm.js";
 
+ const ringtone = new Audio("animal.mp3");
+
 export const dt = document.querySelector(".dt");
 export const tm = document.querySelector(".tm");
 export const selectMenu = document.querySelectorAll("select");
