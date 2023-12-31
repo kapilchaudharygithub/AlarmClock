43ebe2233dd6376485ec55c5d8c73d2b5ccd1cb9
Kapil Chaudhary <153476703+kapilchaudharygithub@users.noreply.github.com>
2023-12-31 14:37:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ import { Alarm } from "./JS/alarm.js";
 export const dt = document.querySelector(".dt");
 export const tm = document.querySelector(".tm");
 export const selectMenu = document.querySelectorAll("select");
-export const ringtone = new Audio("./audio/Alarm-Ringtone.mp3");
 
 Theme();
 Clock();
